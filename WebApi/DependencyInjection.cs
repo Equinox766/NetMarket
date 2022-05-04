7e8a9f0626882fc3182f60355efbb8267dffb22a
Equinox766 <Equinox766@gmail.com>
2022-05-04 11:25:31 -0400

    Agregar archivos de proyecto.

---
+++ WebApi/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogic.Logic;
+using Core.Interfaces;
+
+namespace WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration) {
+            services.AddTransient<IProductoRepository, ProductoRepository>();
+            return services;
+        }
+    }
+}
